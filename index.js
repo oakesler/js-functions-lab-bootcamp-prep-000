281401332f695f16e501a7240509746c84e0074c
Olivia Kesler <oakesler@protonmail.ch>
2018-11-27 21:09:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday) {
-  return `It's ${days} days until ${holiday}!`
+  return `It\'s ${days} days until ${holiday}!`
 }
   
